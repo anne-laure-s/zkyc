8075e7ebc8c85281cf48b22ec40e1e1ba53f3bb7
Anne-Laure <al.schmitt@protonmail.com>
2026-02-22 18:49:06 -0500

    Issuer: remove useless functions

--- src/issuer/mod.rs
+++ src/issuer/mod.rs
@@ -1,15 +1,1 @@
-use rand::Rng;
-
-use crate::{core::credential::Credential, schnorr::signature::Signature};
-
 pub mod keys;
-
-pub fn random_and_sign(rng: &mut impl Rng) -> (Credential, Signature) {
-    let credential = Credential::random_with_issuer(&keys::secret(), rng);
-    let signature = credential.sign(&keys::secret());
-    (credential, signature)
-}
-
-pub fn send(_credential: Credential, _signature: Signature) {
-    todo!()
-}
