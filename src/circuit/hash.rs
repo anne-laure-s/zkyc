d4e9b625a8a2a72cfa923751b7361ce068d56b90
Anne-Laure <al.schmitt@protonmail.com>
2026-03-23 20:56:01 +0100

    Circuit/Hash: add helper functions for Merkle

--- src/circuit/hash.rs
+++ src/circuit/hash.rs
@@ -1,18 +1,38 @@
 use anyhow::Ok;
 use plonky2::{
     field::extension::Extendable,
-    hash::hash_types::RichField,
-    iop::{target::Target, witness::Witness},
+    hash::{
+        hash_types::{HashOutTarget, RichField},
+        poseidon::PoseidonHash,
+    },
+    iop::{
+        target::{BoolTarget, Target},
+        witness::Witness,
+    },
     plonk::circuit_builder::CircuitBuilder,
 };
 
 use crate::encoding::{self, LEN_HASH};
 
+// TODO: some functions are already existing for hash, maybe it’s relevant to use them
 pub type HashTarget = encoding::Hash<Target>;
+
+impl From<HashOutTarget> for HashTarget {
+    fn from(value: HashOutTarget) -> Self {
+        encoding::Hash(value.elements)
+    }
+}
 
 pub trait CircuitBuilderHash<F: RichField + Extendable<D>, const D: usize> {
     fn add_virtual_hash_target(&mut self) -> HashTarget;
     fn register_hash_public_input(&mut self, target: HashTarget);
+    fn connect_hash(&mut self, target1: HashTarget, target2: HashTarget);
+    fn merge_left_right(
+        &mut self,
+        node: HashTarget,
+        is_left: BoolTarget,
+        neighbor: HashTarget,
+    ) -> HashTarget;
 }
 pub trait PartialWitnessHash<F: RichField>: Witness<F> {
     fn get_hash_target(&self, target: HashTarget) -> encoding::Hash<F>;
@@ -35,6 +55,29 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilderHash<F, D>
         for t in target.0.into_iter() {
             self.register_public_input(t);
         }
+    }
+    fn connect_hash(&mut self, target1: HashTarget, target2: HashTarget) {
+        target1
+            .0
+            .into_iter()
+            .zip(target2.0)
+            .for_each(|(t1, t2)| self.connect(t1, t2));
+    }
+
+    fn merge_left_right(
+        &mut self,
+        node: HashTarget,
+        is_left: BoolTarget,
+        neighbor: HashTarget,
+    ) -> HashTarget {
+        let left =
+            std::array::from_fn::<_, LEN_HASH, _>(|i| self._if(is_left, node.0[i], neighbor.0[i]));
+        let right =
+            std::array::from_fn::<_, LEN_HASH, _>(|i| self._if(is_left, neighbor.0[i], node.0[i]));
+        let mut buffer = Vec::with_capacity(2 * LEN_HASH);
+        buffer.extend_from_slice(&left);
+        buffer.extend_from_slice(&right);
+        self.hash_n_to_hash_no_pad::<PoseidonHash>(buffer).into()
     }
 }
 
