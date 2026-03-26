b8c351949fb5e1a98afd87e9ffa89fe464268682
Anne-Laure <al.schmitt@protonmail.com>
2026-03-26 17:09:13 +0100

    Bank: add verify_client_proof

--- src/bank/mod.rs
+++ src/bank/mod.rs
@@ -15,3 +15,22 @@ pub fn nonce() -> String {
 pub fn service() -> String {
     String::from("ZBanK")
 }
+
+pub fn verify_client_proof(
+    circuit: &Circuit,
+    proof: ZkProof,
+    // claimed pseudonym for the client
+    pseudonym: encoding::Pseudonym<circuit::F>,
+) -> anyhow::Result<()> {
+    let issuer_root = issuer::database::for_tests::DATABASE.root();
+    let public_inputs = circuit::inputs::Public {
+        cutoff18_days: date::cutoff18_from_today().to_field(),
+        nationality: Nationality::FR.to_field(),
+        issuer_pk: issuer::keys::public().0.to_field(),
+        nonce: nonce().to_field(),
+        service: service().to_field(),
+        pseudonym,
+        merkle_root: issuer_root,
+    };
+    circuit::verify(&circuit.circuit, proof, public_inputs)
+}
